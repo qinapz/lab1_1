460a1ec9c8cacfcb8f974c074921ea193614c879
Famulski.Michal <187684@edu.p.lodz.pl>
2018-03-06 18:38:44 +0100

    Dodanie klasy Discount

---
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/Discount.java
@@ -0,0 +1,72 @@
+package pl.com.bottega.ecommerce.sales.domain.offer;
+
+import java.math.BigDecimal;
+
+public class Discount {
+
+    private String discountCause;
+
+    private Money discount;
+
+    public String getDiscountCause() {
+        return discountCause;
+    }
+
+    public void setDiscountCause(String discountCause) {
+        this.discountCause = discountCause;
+    }
+
+    public BigDecimal getDiscountValue() {
+        return discount.getCost();
+    }
+
+    public Money getDiscount() {
+        return discount;
+    }
+
+    public Discount(String discountCause, BigDecimal discount) {
+        super();
+        this.discountCause = discountCause;
+        this.discount = new Money(discount);
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((discount == null) ? 0 : discount.hashCode());
+        result = prime * result + ((discountCause == null) ? 0 : discountCause.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Discount other = (Discount) obj;
+        if (discount == null) {
+            if (other.discount != null)
+                return false;
+        } else if (!discount.equals(other.discount))
+            return false;
+        if (discountCause == null) {
+            if (other.discountCause != null)
+                return false;
+        } else if (!discountCause.equals(other.discountCause))
+            return false;
+        return true;
+    }
+
+    public BigDecimal computeDiscount() {
+        BigDecimal discountValue = new BigDecimal(0);
+        if (discount != null) {
+            discountValue = discountValue.subtract(discount.getCost());
+        }
+        return discountValue;
+    }
+
+}
