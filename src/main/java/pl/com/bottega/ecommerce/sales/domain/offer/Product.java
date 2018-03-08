9e0ad7c0b879faa83235bd2fb8e79b4a6a104604
rogo6 <piotrrogozinski95@gmail.com>
2018-03-08 20:14:49 +0100

    Added refactored version of product hashCode method

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/Product.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/Product.java
@@ -35,4 +35,15 @@ public class Product {
     public String getProductType() {
         return productType;
     }
+    
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + (productName == null ? 0 : productName.hashCode());
+        result = prime * result + (productPrice == null ? 0 : productPrice.hashCode());
+        result = prime * result + (productId == null ? 0 : productId.hashCode());
+        result = prime * result + (productType == null ? 0 : productType.hashCode());
+        return result;
+    }
 }
