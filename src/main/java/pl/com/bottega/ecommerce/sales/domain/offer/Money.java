ee24818e2d90d10b18f0c538e71a5aa8078c4bfb
rogo6 <piotrrogozinski95@gmail.com>
2018-03-08 20:21:34 +0100

    Added refactored version of Money hashCode method

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/Money.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/Money.java
@@ -28,4 +28,12 @@ public class Money {
 		this.currency=currency;
 	}
 	
+	@Override
+	public int hashCode() {
+       final int prime = 31;
+       int result = 1;
+       result = prime * result + (value == null ? 0 : value.hashCode());
+       result = prime * result + (currency == null ? 0 : currency.hashCode());
+       return result;
+	}
 }
